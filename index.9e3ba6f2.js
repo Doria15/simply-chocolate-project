9ea71ed040872a3f77d5b96b56aed85c11548036
Doria15 <134120554+Doria15@users.noreply.github.com>
2023-09-06 15:18:13 +0000

    Deploying to gh-pages from @ Doria15/simply-chocolate-project@678046788062c900c621c06bb3a789ac27aca530 🚀

---
+++ index.9e3ba6f2.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".mySwiper",{slidesPerView:3,spaceBetween:30,freeMode:!0,pagination:{el:".swiper-pagination",clickable:!0}});
+//# sourceMappingURL=index.9e3ba6f2.js.map
--- index.cf742d5a.js
+++ index.cf742d5a.js
@@ -1,2 +1,2 @@
 !function(){var e={openModalBtn:document.querySelector("[reviews-modal-open]"),closeModalBtn:document.querySelector("[reviews-modal-close]"),modal:document.querySelector("[reviews-data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
-//# sourceMappingURL=index.355d5e4f.js.map
+//# sourceMappingURL=index.cf742d5a.js.map
