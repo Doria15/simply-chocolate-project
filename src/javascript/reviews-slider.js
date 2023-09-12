4131a64e1e4b7074dc7c38b78c6836bd19e65673
Paulo <pau.solanoy@gmail.com>
2023-09-12 22:32:19 +0800

    update

--- src/javascript/reviews-slider.js
+++ src/javascript/reviews-slider.js
@@ -6,4 +6,18 @@ var swiper = new Swiper('.mySwiper', {
     el: '.swiper-pagination',
     clickable: true,
   },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 16,
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 28,
+    },
+  },
 });
